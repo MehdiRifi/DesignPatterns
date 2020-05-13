0949c7690a0c3fe5ccd7023c22b37a3236b1bf6b
Mehdi Rifi <mehdirifi51@gmail.com>
2020-05-13 23:53:27 +0000

    feat(iterator): add abstract and concrete collection

---
+++ DesignPatterns/Behavioral/Iterator/Collection/ITreeCollection.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace DesignPatterns.Behavioral.Iterator
+{
+    public interface ITreeCollection
+    {
+        IIterator<TreeItem> GetDepthFirstIterator();
+    }
+}
---
+++ DesignPatterns/Behavioral/Iterator/Collection/TreeItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Behavioral.Iterator
+{
+    public class TreeItem
+    {
+        public string Name { get; set; }
+        public TreeItem Parent { get; set; }
+        public IList<TreeItem> Childs { get; set; }
+        public bool HasChild
+        {
+            get
+            {
+                return Childs != null && Childs.Count > 0;
+            }
+        }
+        public TreeItem(string name)
+        {
+            Name = name;
+            Childs = new List<TreeItem>();
+        }
+        public void AddChilds(params TreeItem[] items)
+        {
+            foreach (var node in items)
+            {
+                Childs.Add(node);
+            }
+        }
+    }
+}
